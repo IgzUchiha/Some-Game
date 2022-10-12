65df88dcf2f7cfea4eb00b7abe87d7f36121ebda
IgzUchiha <iamsupervegeta14@gmail.com>
2022-10-12 11:35:58 -0700

    creative destruction2

--- item.py
+++ item.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 # from web3 import Web3
 import random
 
+STARTING_POSITION = [(0, 0)]
 UP = "space"
 DOWN = ""
 
@@ -14,6 +15,11 @@ class Item(Turtle):
         self.shapesize(stretch_len=1, stretch_wid=1)
         self.color("blue")
         self.speed("fastest")
+        self.segments = []
+
+    def create_item(self):
+        for position in STARTING_POSITION:
+            self.goto(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -22,5 +28,9 @@ class Item(Turtle):
             self.segments[seg_num].goto(new_x, new_y)
 
     def up(self):
-        if self.head.heading() != DOWN:
+        if self.dome.heading() != DOWN:
             self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.dome.heading() != UP:
+            self.segments[0].setheading(DOWN)
